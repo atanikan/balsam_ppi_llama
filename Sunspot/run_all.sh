29ae089d7e4a58f101975d620205d893560411ed
Aditya Tanikanti <aditya.tanikanti@gmail.com>
2023-11-10 14:55:01 -0600

    run file edits

--- Sunspot/run_all.sh
+++ Sunspot/run_all.sh
@@ -1,5 +1,9 @@
+echo Starting work flow
 python3 define_app.py
+echo Defined Balsam App
 python3 define_jobs.py
-sleep 60
+echo Creating Balsam jobs
+sleep 120
+echo Building dot file
 python3 build_dot_file.py
 
